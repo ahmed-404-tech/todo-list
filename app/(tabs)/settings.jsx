0c87e6582a6ab000a0b43df1aea0ea3c31f1e5df
Ahmed Abdulsalam <ahmed.tech002@gmail.com>
2025-02-01 08:45:30 +0300

    Change App Theme

--- app/(tabs)/settings.jsx
+++ app/(tabs)/settings.jsx
@@ -1,14 +1,75 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Switch,
+  Text,
+  View,
+} from "react-native";
+import { useContext } from "react";
+import { ThemeContext } from "../contexts/ThemeContext";
 
 const settings = () => {
+  const { theme, toggleTheme, isEnabled } = useContext(ThemeContext);
+
   return (
-    <View>
-      <Text>settings</Text>
-    </View>
-  )
-}
+    <SafeAreaView style={{ flex: 1 }}>
+      <ScrollView
+        contentContainerStyle={[
+          styles.container,
+          { backgroundColor: theme.colors.background },
+        ]}
+      >
+        <Text style={styles.headerText}>Settings</Text>
+        <View
+          style={[
+            styles.switchView,
+            { borderColor: theme.colors.background_300 },
+          ]}
+        >
+          <Text style={[styles.settingText, { color: theme.colors.text }]}>
+            Dark Mode
+          </Text>
+          <Switch onChange={toggleTheme} value={isEnabled} />
+        </View>
+        <View
+          style={[
+            styles.switchView,
+            { borderColor: theme.colors.background_300 },
+          ]}
+        >
+          <Text style={[styles.settingText, { color: theme.colors.text }]}>
+            Noffications
+          </Text>
+          <Switch onChange={toggleTheme} value={isEnabled} />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
-export default settings
+export default settings;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    gap: 20,
+  },
+  headerText: {
+    fontSize: 28,
+    fontWeight: "bold",
+    marginBottom: 40,
+  },
+  switchView: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 20,
+    alignItems: "center",
+  },
+  settingText: {
+    fontSize: 20,
+  },
+});
---
+++ app/themes/index.js
@@ -0,0 +1,25 @@
+export const lightTheme = {
+    colors: {
+        background: '#FFFFFF',
+        background_200: '#f5f5f5',
+        background_300: '#cccccc',
+        primary: '#0077C2',
+        primary_200: "#59a5f5",
+        accent: '#00BFFF',
+        text: '#333333',
+        text_200: '#5c5c5c',
+    }
+}
+
+export const darkTheme = {
+    colors: {
+        background: '#1E1E1E',
+        background_200: '#2d2d2d',
+        background_300: '#454545',
+        primary: '#0085ff',
+        primary_200: "#69b4ff",
+        accent: '#006fff',
+        text: '#FFFFFF',
+        text_200: '#9e9e9e',
+    }
+}
